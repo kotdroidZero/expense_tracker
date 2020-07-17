4518580657b0be1e7d53fc6c4db950f93401d5ef
Pushkar <d.errorfound@gmail.com>
2020-07-18 04:03:28 +0530

    first commit

---
+++ lib/transaction.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class Transaction {
+  final String id;
+  final String title;
+  final double amount;
+  final DateTime date;
+  Transaction({this.id, this.title, this.amount, this.date});
+}
